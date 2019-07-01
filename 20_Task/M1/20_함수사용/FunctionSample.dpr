d06f0f3fb8b2d113a4e1ae008172171af0e55c56
hjfactory@gmail.com <hjfactory>
2019-07-01 11:09:58 +0900

    미션1 프로젝트 코드 등록

---
+++ 20_Task/M1/20_함수사용/FunctionSample.dpr
@@ -0,0 +1,14 @@
+program FunctionSample;
+
+uses
+  Vcl.Forms,
+  FunctionForm in 'FunctionForm.pas' {Form2};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm2, Form2);
+  Application.Run;
+end.
