a578849cbce9daa1f68f03afb396c6956341a09e
docessonhos@naver.com <JJ-HH>
2019-07-01 17:35:41 +0900

    Commit Test(강지호)

--- 20_Task/M1/10_Variable/VariableForm.pas
+++ 20_Task/M1/10_Variable/VariableForm.pas
@@ -1,7 +1,7 @@
 unit VariableForm;
 
 interface
-
+       //git test
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
