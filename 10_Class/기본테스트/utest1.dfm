34fe9022565a223434fd0e0c59df563d8ad04723
wkkim <kwk@embarcadero.kr>
2019-07-03 08:51:17 +0900

    델파이기본소스2일차

--- 10_Class/기본테스트/utest1.dfm
+++ 10_Class/기본테스트/utest1.dfm
@@ -14,4 +14,12 @@ object Form217: TForm217
   OnActivate = FormActivate
   PixelsPerInch = 96
   TextHeight = 13
+  object Button1: TButton
+    Left = 24
+    Top = 24
+    Width = 75
+    Height = 25
+    Caption = 'Button1'
+    TabOrder = 0
+  end
 end
--- 10_Class/기본테스트/utest1.pas
+++ 10_Class/기본테스트/utest1.pas
@@ -4,11 +4,13 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
 
 type
   TForm217 = class(TForm)
+    Button1: TButton;
     procedure FormActivate(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -22,9 +24,14 @@ implementation
 
 {$R *.dfm}
 
+procedure TForm217.Button1Click(Sender: TObject);
+begin
+ close;
+end;
+
 procedure TForm217.FormActivate(Sender: TObject);
 begin
-  caption := inttostr(application.ComponentCount);
+  caption := inttostr(ComponentCount);
 end;
 
 end.
