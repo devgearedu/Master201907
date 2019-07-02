03d4d7e5a4cd3e42b9ec1b0e350fbaecbf3db1fa
SeokJinChe <52394395+SeokJinChe@users.noreply.github.com>
2019-07-02 21:52:53 +0900

    Add files via upload

--- 20_Task/M1/10_Variable/VariableForm.pas
+++ 20_Task/M1/10_Variable/VariableForm.pas
@@ -1,7 +1,7 @@
 unit VariableForm;
 
 interface
- // 깃허브 테스트
+
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
@@ -16,10 +16,8 @@ type
     procedure Button1Click(Sender: TObject);
   private
     // 이 폼(유닛)에서만 사용하는 변수와 함수를 선언
-
-    { TODO : (1) 정수 형 FSum 변수를 선언하세요. }
-
-    function AddNum(ANum: Integer): Integer;
+    FSum: Integer;  //합산에 사용할 변수 FSum
+    function AddNum(ANum: Integer): Integer; //입력값을 더해주는 function
   public
     // 다른 유닛에서 참조할 수 있는 변수와 함수 선언
   end;
@@ -33,20 +31,18 @@ implementation
 
 function TForm1.AddNum(ANum: Integer): Integer;
 begin
-  { TODO :
-      (2) FSum 변수에 파라메터 ANum 값을 더합니다.
-          FSum 변수 반환 }
-  Result := 0;
+  FSum := FSum + ANum; //기존 FSum값에 새로 입력한 값을 더한다.
+  Result := Fsum; // 더해진 값을 반환한다.
 end;
 
 procedure TForm1.Button1Click(Sender: TObject);
 var
   Num, Sum: Integer;  // 이곳에 선언된 변수는 이 함수에서만 사용합니다.
 begin
-  Num := StrToInt(edtNum.Text);
-  Sum := AddNum(Num);
+  Num := StrToInt(edtNum.Text); //edtNum에 입력한 값을 Num에 저장한다.
+  Sum := AddNum(Num);  //AddNum function에 Num값을 넘겨주고받은 값을 Sum에 저장.
 
-  edtSum.Text := IntToStr(Sum);
+  edtSum.Text := IntToStr(Sum); //결과값을 출력한다.
 end;
 
 end.
